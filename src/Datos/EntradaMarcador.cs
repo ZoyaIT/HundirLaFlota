beb129e02583a80bf641232f4f993933236495ab
Zoya <zoya.ahmed@estudiant.fjaverianas.com>
2026-04-08 22:54:32 +0200

    Delete src directory

--- src/Datos/EntradaMarcador.cs
+++
@@ -1,9 +0,0 @@
-namespace HundirLaFlota.Datos;
-
-public class EntradaMarcador
-{
-    public string Nombre { get; set; } = "";
-    public int Disparos { get; set; }
-    public double Precision { get; set; }
-    public double Puntuacion { get; set; }
-}
